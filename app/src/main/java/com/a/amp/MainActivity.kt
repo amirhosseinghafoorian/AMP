de342b621c9606dcde902fb0c3de4e94cebad6c7
aminshojaei <m.aminshojaei.b@gmail.com>
2020-08-17 01:06:45 +0430

    Navigation component(safe args)

--- app/src/main/java/com/a/amp/MainActivity.kt
+++ app/src/main/java/com/a/amp/MainActivity.kt
@@ -2,10 +2,31 @@ package com.a.amp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.frag_login.*
+import kotlinx.android.synthetic.main.fragment_signup.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.splash)
+        setContentView(R.layout.activity_main)
+
+
+
+//        login_box_3.setOnClickListener(){
+//            var user1 = userName.text.toString()
+//            var pass1 = passWord.text.toString()
+//            var fragmentManager = supportFragmentManager
+//            fragmentManager.beginTransaction().replace(R.id.frameLayout, frag2).addToBackStack("frag2").commit()
+//        }
+//
+//        signup_box_5.setOnClickListener(){
+//            var fragmentManager = supportFragmentManager
+//            fragmentManager.beginTransaction().replace(R.id.frag1).
+//        }
+
     }
 }
