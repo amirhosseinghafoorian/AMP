26cf234a290af8034261e42f5b52d3ca9c5c0e07
aminshojaei <m.aminshojaei.b@gmail.com>
2020-08-23 00:10:31 +0430

    added bottom sheet and dialog

--- app/src/main/java/com/a/amp/ArticleFragment.kt
+++ app/src/main/java/com/a/amp/ArticleFragment.kt
@@ -1,10 +1,15 @@
 package com.a.amp
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_profile.*
+
 private const val ARG_PARAM1 = "ID"
 class ArticleFragment : Fragment() {
     private var param1: Int = 0
@@ -21,6 +26,21 @@ class ArticleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_article, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val dialog = Dialog(requireContext())
+        val view = layoutInflater.inflate(R.layout.comment_dialog,null)
+
+        dialog.setContentView(view)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+//        dialog.window?.setLayout(1000,1000)
+
+        profile_cv_btn_1.setOnClickListener {
+            dialog.show()
+        }
     }
 
     companion object {
--- app/src/main/java/com/a/amp/ProfileFragment.kt
+++ app/src/main/java/com/a/amp/ProfileFragment.kt
@@ -1,12 +1,18 @@
 package com.a.amp
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.dialog.MaterialDialogs
 import kotlinx.android.synthetic.main.fragment_profile.*
+import kotlinx.android.synthetic.main.writing_cv.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,6 +35,22 @@ class ProfileFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val bottonSheetDialog = BottomSheetDialog(requireContext())
+        val  view = layoutInflater.inflate(R.layout.bottom_sheet,null)
+
+        bottonSheetDialog.setContentView(view)
+
+        profile_cv_btn_1.setOnClickListener {
+            bottonSheetDialog.show()
+        }
+
+
     }
 
     override fun onCreateView(
@@ -43,6 +65,9 @@ class ProfileFragment : Fragment() {
 
             }
         }
+
+
+
     }
 
     companion object {
