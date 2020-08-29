36aeb57e9a011056f823e95fd6457fc7d0e8612c
aminshojaei <m.aminshojaei.b@gmail.com>
2020-08-29 15:04:06 +0430

    added cards icons

--- app/src/main/java/com/a/amp/WritingCvDataItem.kt
+++ app/src/main/java/com/a/amp/WritingCvDataItem.kt
@@ -1,3 +1,3 @@
 package com.a.amp
 
-data class WritingCvDataItem(var title: String,var text: String, var name: String, var days: String, var id: Int)
+data class  WritingCvDataItem(var title: String,var text: String, var name: String, var days: String, var id: Int)
